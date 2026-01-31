acef46ae1766ef03abd9b6747f63038bb678e3c1
Laura Martin Davis <lauraomartin@icloud.com>
2026-01-31 13:02:09 -0500

    Initial Commit

---
+++ You Are Awesome 2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  You Are Awesome 2
+//
+//  Created by Laura Becca Davis on 31/01/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ You Are Awesome 2/You_Are_Awesome_2App.swift
@@ -0,0 +1,17 @@
+//
+//  You_Are_Awesome_2App.swift
+//  You Are Awesome 2
+//
+//  Created by Laura Becca Davis on 31/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct You_Are_Awesome_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
