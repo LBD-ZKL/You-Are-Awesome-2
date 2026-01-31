014db04b23fb075c97d4c823243f8fe5bb91c10b
Laura Martin Davis <lauraomartin@icloud.com>
2026-01-31 13:06:09 -0500

    New photos

--- You Are Awesome 2/ContentView.swift
+++ You Are Awesome 2/ContentView.swift
@@ -1,24 +1,52 @@
 //
 //  ContentView.swift
-//  You Are Awesome 2
+//  YouAreAwesome
 //
-//  Created by Laura Becca Davis on 31/01/2026.
+//  Created by Laura Becca Davis on 17/01/2026.
 //
 
 import SwiftUI
 
 struct ContentView: View {
+    @State private var message = ""
+    @State private var imageName = ""
     var body: some View {
+        
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Spacer()
+             
+           Image(imageName)
+                .resizable()
+                .scaledToFit()
+                .clipShape(RoundedRectangle(cornerRadius: 30))
+                .shadow(radius: 30)
+              
+            Text(message)
+                .font(.largeTitle)
+                .fontWeight(.heavy)
+                .foregroundStyle(.red)
+            
+            Spacer()
+            
+            Button("Press ME!") {
+                let message1 = "You are Awesome!"
+                let message2 = "You are Great!"
+             
+                
+                message = (message == message1 ? message2 : message1)
+                imageName = ( imageName == "image0" ? "image1" : "image0" )
+            }
+            .buttonStyle(.borderedProminent)
+            .font(.title2)
+            
+      
+            
         }
         .padding()
+        
     }
 }
-
+ 
 #Preview {
     ContentView()
 }
